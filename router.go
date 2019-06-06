511b1f5a7c5a6d9d37fdb8260b3c09d58d5bf1cd
Aleksandar Dimitrov <tartaruga@tuta.io>
2019-06-06 19:41:25 +0200

    Create path prefix and sub routers

--- router.go
+++ router.go
@@ -8,11 +8,13 @@ import (
 )
 
 func createRouter() {
-	r := mux.NewRouter()
-	r.HandleFunc("/api/deck/new", getNewDeck).Methods("GET")
-	r.HandleFunc("/api/deck/{id}/get", findDeckByID).Methods("GET")
-	r.HandleFunc("/api/deck/{id}/shuffle", shuffleDeckByID).Methods("GET")
-	r.HandleFunc("/api/deck/shuffle/new", getNewShuffledDeck).Methods("GET")
+	router := mux.NewRouter()
+	router.StrictSlash(true)
+	subrouter := router.PathPrefix("/api/deck").Subrouter()
+	subrouter.HandleFunc("/new", getNewDeck).Methods("GET")
+	subrouter.HandleFunc("/{id}/get", findDeckByID).Methods("GET")
+	subrouter.HandleFunc("/{id}/shuffle", shuffleDeckByID).Methods("GET")
+	subrouter.HandleFunc("/shuffle/new", getNewShuffledDeck).Methods("GET")
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
